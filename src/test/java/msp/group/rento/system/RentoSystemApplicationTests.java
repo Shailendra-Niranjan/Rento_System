f92969518f7bbc6a6bcd755448746cdb6ed736f7
Shailendra-Niranjan <112262132+Shailendra-Niranjan@users.noreply.github.com>
2024-12-12 16:56:46 +0530

    Add files via upload

---
+++ src/test/java/msp/group/rento/system/RentoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package msp.group.rento.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
